c2f1221cf1be10b70f000e5c0ff58b6f2df56810
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-05-02 19:27:16 +0200

    :art: Add imports

--- src/move/__init__.py
+++ src/move/__init__.py
@@ -1,2 +1,6 @@
 __license__ = "MIT"
 __version__ = (1, 0, 0)
+__all__ = ["analysis", "conf", "data", "models", "VAE"]
+
+from move import analysis, conf, data, models
+from move.models.vae import VAE

f6c49072248e50e1f0c1b36eb4f87cb6919d074a
Valentas <jct113@alumni.ku.dk>
2022-07-12 10:43:22 +0200

    update make_random_data.py to save in data/ folder

--- make_random_data.py
+++ make_random_data.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import re
 import random
 from collections import defaultdict
+import os 
 
 def write_cat(f_name, size, minimum, maximum, name):
     data = np.random.randint(minimum, maximum + 1, size = size)
@@ -25,37 +26,41 @@ def write_con(f_name, size, m, s, zero, name):
         for i in range(data.shape[0]):
             o.write(str(i) + "\t" + "\t".join([str(round(np.abs(j))) for j in data[i,:]]) + "\n")
 
+# Creating data folder
+isExist = os.path.exists('data/')
+if not isExist:
+    os.makedirs('data/')     
 
-with open("data_for_git/baseline_ids.txt", "w") as f:
+with open("data/baseline_ids.txt", "w") as f:
     f.write("\n".join([str(i) for i in range(789)]))
 
-cat_f =  "data_for_git/baseline_categorical.tsv"
+cat_f =  "data/baseline_categorical.tsv"
 write_cat(cat_f, (789,42), 0, 4, "clinical_categorical_")
 
-geno_f =  "data_for_git/diabetes_genotypes.tsv"
+geno_f =  "data/diabetes_genotypes.tsv"
 write_cat(geno_f, (789,393), 0, 2, "SNP_")
 
-drug_f =  "data_for_git/baseline_drugs.tsv"
+drug_f =  "data/baseline_drugs.tsv"
 write_cat(drug_f, (789,20), 0, 1, "drug_")
 
 # Generate continous data
-clin_f = "data_for_git/baseline_continuous.tsv"
+clin_f = "data/baseline_continuous.tsv"
 write_con(clin_f, [789, 76], 10, 3, 0.1, "clinical_continuous_")
 
-diet_wearables_f = "data_for_git/baseline_diet_wearables.tsv"
+diet_wearables_f = "data/baseline_diet_wearables.tsv"
 write_con(diet_wearables_f, [789, 74], 100, 50, 0.2, "diet_wearables_")
 
-pro_f = "data_for_git/baseline_proteomic_antibodies.tsv"
+pro_f = "data/baseline_proteomic_antibodies.tsv"
 write_con(pro_f, [789, 373], 15, 7, 0.25, "protein_")
 
-target_mata_f = "data_for_git/baseline_target_metabolomics.tsv"
+target_mata_f = "data/baseline_target_metabolomics.tsv"
 write_con(target_mata_f, [789, 119], 10, 5, 0.2, "targeted_metabolomics_")
 
-untarget_mata_f = "data_for_git/baseline_untarget_metabolomics.tsv"
+untarget_mata_f = "data/baseline_untarget_metabolomics.tsv"
 write_con(untarget_mata_f, [789, 238], 10, 5, 0.3, "untargeted_metabolomics_")
 
-trans_f = "data_for_git/baseline_transcriptomics.tsv"
+trans_f = "data/baseline_transcriptomics.tsv"
 write_con(trans_f, [789, 6018], 20, 15, 0.35, "transcriptomics_")
 
-metagen_f = "data_for_git/baseline_metagenomics.tsv"
+metagen_f = "data/baseline_metagenomics.tsv"
 write_con(metagen_f, [789, 1463], 15, 10, 0.6, "metagenomics_")

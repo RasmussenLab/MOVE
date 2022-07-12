5710d5d88f76f8475fae0f2ebdc0cdf19d1a33b8
Valentas <jct113@alumni.ku.dk>
2022-07-12 14:37:22 +0200

    add Ricardo's dataloaders.py and seed.py files

---
+++ src/move/_utils/seed.py
@@ -0,0 +1,16 @@
+__all__ = ["set_global_seed"]
+
+import random
+from typing import Optional
+
+import numpy as np
+import torch
+
+
+def set_global_seed(seed: Optional[int]) -> None:
+    if seed is None:
+        return
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)

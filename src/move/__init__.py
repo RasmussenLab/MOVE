3578f8c69f5f6e64afefa89fd38234906a9d5b28
Valentas <jct113@alumni.ku.dk>
2022-07-13 21:04:45 +0200

    update __init__.py to new version

--- src/move/__init__.py
+++ src/move/__init__.py
@@ -1,5 +1,5 @@
 __license__ = "MIT"
-__version__ = (1, 0, 5)
+__version__ = (1, 0, 6)
 __all__ = ["analysis", "conf", "data", "models", "training", "VAE"]
 
 from move import analysis, conf, models, training, data

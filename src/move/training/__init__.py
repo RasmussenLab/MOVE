954e662da6d01945dff3c3aff1ecd3303c6f2edc
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-09-06 16:04:35 +0200

    :art: Add missing __init__

---
+++ src/move/training/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "optimize_reconstruction",
+    "optimize_stability",
+    "train_model_association",
+    "train_model",
+]
+
+from move.training.train import (
+    optimize_reconstruction,
+    optimize_stability,
+    train_model_association,
+    train_model,
+)

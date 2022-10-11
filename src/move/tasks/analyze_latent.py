163c84d3dc5897a8aab911c7f1269bac4155e034
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-10-11 17:07:07 +0200

    :sparkles: Plot loss curves

--- src/move/tasks/analyze_latent.py
+++ src/move/tasks/analyze_latent.py
@@ -7,7 +7,8 @@ import hydra
 import numpy as np
 import torch
 
-from move.conf.schema import AnalyzeLatentsConfig, MOVEConfig
+import move.visualization as viz
+from move.conf.schema import AnalyzeLatentConfig, MOVEConfig
 from move.core.logging import get_logger
 from move.data import io
 from move.data.dataloaders import MOVEDataset, make_dataloader
@@ -20,12 +21,12 @@ def analyze_latent(config: MOVEConfig):
 
     logger = get_logger(__name__)
     logger.info("Beginning task: analyze latent space")
-    task_config = cast(AnalyzeLatentsConfig, config.task)
+    task_config = cast(AnalyzeLatentConfig, config.task)
 
     output_path = Path(config.data.processed_data_path) / "latent_space"
     output_path.mkdir(exist_ok=True, parents=True)
 
-    cat_list, cat_names, con_list, con_names = io.load_preprocessed_data(
+    cat_list, _, con_list, _ = io.load_preprocessed_data(
         Path(config.data.interim_data_path),
         config.data.categorical_names,
         config.data.continuous_names,
@@ -60,4 +61,8 @@ def analyze_latent(config: MOVEConfig):
             train_dataloader=train_dataloader,
         )
         torch.save(model.state_dict(), model_path)
+        logger.debug("Generating plot: loss curves")
+        fig = viz.plot_loss_curves(output[:-1])
+        fig.savefig(str(output_path / "loss_curve.png"), bbox_inches="tight")
+
     model.eval()

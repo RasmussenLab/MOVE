cc6a1c5f8b23740825d78dc01ac8ee86216cff99
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-10-14 20:02:40 +0200

    :sparkles: Add metrics boxplot function

--- src/move/visualization/__init__.py
+++ src/move/visualization/__init__.py
@@ -1,7 +1,10 @@
 __all__ = [
+    "color_cycle",
     "plot_latent_space_with_cat",
     "plot_latent_space_with_con",
     "plot_loss_curves",
+    "plot_metrics_boxplot",
+    "style_settings",
     "LOSS_LABELS",
 ]
 
@@ -11,3 +14,5 @@ from move.visualization.latent_space import (
     plot_latent_space_with_con,
 )
 from move.visualization.loss_curves import plot_loss_curves, LOSS_LABELS
+from move.visualization.metrics import plot_metrics_boxplot
+from move.visualization.style import color_cycle, style_settings

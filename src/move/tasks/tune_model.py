7f80fd41875fe91e2e00a1a7bb25b2a626e24275
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-10-20 16:18:38 +0200

    :construction: Record reconstruction stats

--- src/move/tasks/tune_model.py
+++ src/move/tasks/tune_model.py
@@ -1,16 +1,33 @@
 __all__ = ["tune_model"]
 
 from pathlib import Path
-from typing import Union
+from typing import Any, cast
 
 import hydra
+import numpy as np
+import pandas as pd
 from hydra.core.hydra_config import HydraConfig
 from hydra.types import RunMode
+from matplotlib.cbook import boxplot_stats
 
+from move.analysis.metrics import (
+    calculate_accuracy,
+    calculate_cosine_similarity,
+)
+from move.core.typing import FloatArray
 from move.data import io
-from move.conf.schema import MOVEConfig
+from move.data.dataloaders import MOVEDataset, make_dataloader
+from move.conf.schema import MOVEConfig, TuneModelConfig
 from move.core.logging import get_logger
 from move.models.vae import VAE
+
+
+def _get_record(values: FloatArray, **kwargs) -> dict[str, Any]:
+    record = kwargs
+    bxp_stats, *_ = boxplot_stats(values)
+    bxp_stats.pop("fliers")
+    record.update(bxp_stats)
+    return record
 
 
 def tune_model(config: MOVEConfig) -> float:
@@ -24,6 +41,7 @@ def tune_model(config: MOVEConfig) -> float:
     logger = get_logger(__name__)
     logger.info(f"Beginning task: tune model {job_num}")
     logger.info(f"Job name: {hydra_config.job.override_dirname}")
+    task_config = cast(TuneModelConfig, config.task)
 
     raw_data_path = Path(config.data.raw_data_path)
     interim_path = Path(config.data.interim_data_path)
@@ -38,5 +56,67 @@ def tune_model(config: MOVEConfig) -> float:
         config.data.categorical_names,
         config.data.continuous_names,
     )
+    train_dataloader = make_dataloader(
+        cat_list,
+        con_list,
+        shuffle=True,
+        batch_size=task_config.batch_size,
+        drop_last=True,
+    )
+    test_dataset = cast(MOVEDataset, train_dataloader.dataset)
+
+    assert task_config.model is not None
+    model: VAE = hydra.utils.instantiate(
+        task_config.model,
+        continuous_shapes=test_dataset.con_shapes,
+        categorical_shapes=test_dataset.cat_shapes,
+    )
+    logger.debug(f"Model: {model}")
+
+    logger.debug("Training model")
+    hydra.utils.call(
+        task_config.training_loop,
+        model=model,
+        train_dataloader=train_dataloader,
+    )
+
+    logger.info("Reconstructing")
+    logger.info("Computing reconstruction metrics")
+    label = [hp.split("=") for hp in hydra_config.job.override_dirname.split(",")]
+    records = []
+    zip_ = zip([train_dataloader], ["train", "test"])
+    for dataloader, split_name in zip_:
+        cat_recons, con_recons = model.reconstruct(dataloader)
+        con_recons = np.split(con_recons, model.continuous_shapes[:-1], axis=1)
+        for cat, cat_recon, dataset_name in zip(
+            cat_list, cat_recons, config.data.categorical_names
+        ):
+            accuracy = calculate_accuracy(cat, cat_recon)
+            record = _get_record(
+                accuracy,
+                job_num=job_num,
+                **dict(label),
+                metric="accuracy",
+                dataset=dataset_name,
+                split=split_name,
+            )
+            records.append(record)
+        for con, con_recon, dataset_name in zip(
+            con_list, con_recons, config.data.continuous_names
+        ):
+            cosine_sim = calculate_cosine_similarity(con, con_recon)
+            record = _get_record(
+                cosine_sim,
+                job_num=job_num,
+                **dict(label),
+                metric="cosine_similarity",
+                dataset=dataset_name,
+                split=split_name,
+            )
+            records.append(record)
+    df_path = output_path / "reconstruction_stats.tsv"
+    header = not df_path.exists()
+    df = pd.DataFrame.from_records(records)
+    df.to_csv(df_path, sep="\t", mode="a", header=header, index=False)
 
     return 0.0

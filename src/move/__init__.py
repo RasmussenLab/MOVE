f3f818cf5724d029a28f9c040a4aa336f0130355
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-10-26 16:00:11 +0200

    :fire: Remove deprecated modules

--- src/move/__init__.py
+++ src/move/__init__.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 __license__ = "MIT"
 __version__ = (1, 2, 0)
-__all__ = ["conf", "data", "models", "training", "VAE"]
+__all__ = ["conf", "data", "models", "training_loop", "VAE"]
 
 
-from move import conf, models, training, data
+from move import conf, models, data
 from move.models.vae import VAE
+from move.training.training_loop import training_loop

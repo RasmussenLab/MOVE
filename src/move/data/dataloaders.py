7362e73b384dc57a7baeed7ec98a8ff199c82d0d
Valentas <jct113@alumni.ku.dk>
2022-11-05 22:12:28 +0100

    Update docstrings to dataloaders

--- src/move/data/dataloaders.py
+++ src/move/data/dataloaders.py
@@ -10,9 +10,23 @@ from torch.utils.data import DataLoader, TensorDataset
 from move.core.typing import BoolArray, FloatArray
 
 
-class MOVEDataset(TensorDataset):
-    "Characterizes a dataset for PyTorch"
+class MOVEDataset(TensorDataset):    
+    """
+    Characterizes a dataset for PyTorch
 
+    Attributes:
+        TensorDataset (class): class to represent data as list of tensors
+        
+    Args:
+        cat_all (torch.Tensor, optional): categorical input matrix (N_patients, N_variables x N_max-classes. Defaults to None.
+        con_all (torch.Tensor, optional): normalized continuous input matrix (N_patients, N_variables). Defaults to None.
+        cat_shapes (list, optional): list of tuples correspoding to number of features (N_variables, N_max-classes) of each categorical class. Defaults to None.
+        con_shapes (list, optional): list of tuples correspoding to number of features (N_variables) of each continuous class. Defaults to None.
+
+    Raises:
+        ValueError: Number of samples between categorical and continuous datasets must match.
+        ValueError: Categorical and continuous data cannot be both empty.
+    """   
     def __init__(
         self,
         cat_all: Optional[torch.Tensor] = None,
@@ -39,11 +53,28 @@ class MOVEDataset(TensorDataset):
         self.con_shapes = con_shapes
 
     def __len__(self) -> int:
+        """
+        Return number of samples samples
+
+        Returns:
+            int: number of samples
+        """  
         return self.num_samples
 
     def __getitem__(
         self, idx: int
     ) -> tuple[Optional[torch.Tensor], Optional[torch.Tensor]]:
+        """
+        Get item based on selected index.
+
+        Args:
+            idx (int): index of item
+
+        Returns:
+            (tuple): a tuple containing:
+                cat_slice (torch.Tensor): categorical features data of selected item
+                con_slice (torch.Tensor): continuous features data of selected item
+        """   
         cat_slice = None if self.cat_all is None else self.cat_all[idx]
         con_slice = None if self.con_all is None else self.con_all[idx]
         return cat_slice, con_slice
@@ -52,6 +83,18 @@ class MOVEDataset(TensorDataset):
 def concat_cat_list(
     cat_list: list[FloatArray],
 ) -> tuple[list[tuple[int, ...]], BoolArray, FloatArray]:
+    """
+    Concatenate a list of categorical data
+
+    Args:
+        cat_list (list[FloatArray]): list with each categorical class data
+
+    Returns:
+        (tuple): a tuple containing:
+            cat_shapes (list[tuple[int, ...]]): list of categorical data classes shapes (N_patients, N_variables, N_max-classes)
+            mask (BoolArray):  mask for patients with no measurments
+            cat_all (FloatArray): 2D array of concatenated patients categorical data
+    """  
     cat_shapes = []
     cat_flat = []
     for cat in cat_list:
@@ -65,6 +108,18 @@ def concat_cat_list(
 def concat_con_list(
     con_list: list[FloatArray],
 ) -> tuple[list[int], BoolArray, FloatArray]:
+    """
+    Concatenate a list of continuous data
+
+    Args:
+        con_list (list[FloatArray]): list with each continuous class data
+
+    Returns:
+        (tuple): a tuple containing:
+            n_con_shapes (list[int]): list of continuous data classes shapes (in 1D) (N_variables)
+            mask (BoolArray):  mask for patients with no measurments
+            con_all (FloatArray): 2D array of concatenated patients continuous data
+    """    
     con_shapes = [con.shape[1] for con in con_list]
     con_all: FloatArray = np.concatenate(con_list, axis=1)
     mask = con_all.sum(axis=1) != 0  # True if row sum is not zero
@@ -76,7 +131,8 @@ def make_dataloader(
     con_list: Optional[list[FloatArray]] = None,
     **kwargs
 ) -> tuple[BoolArray, DataLoader]:
-    """Creates a DataLoader that combines categorical and continuous datasets.
+    """
+    Creates a DataLoader that combines categorical and continuous datasets.
 
     Args:
         cat_list: list of categorical datasets (# samples x # features
@@ -89,8 +145,9 @@ def make_dataloader(
         ValueError: If both inputs are None
 
     Returns:
-        Tuple containing (1) mask to remove rows (samples) with all zeros and
-        (2) DataLoader
+        (tuple): a tuple containing:
+            mask (BoolArray): mask to remove rows (samples) with all zeros
+            dataloader (Dataloader): Dataloader
     """
     if cat_list is None and con_list is None:
         raise ValueError("At least one type of data must be in the input")

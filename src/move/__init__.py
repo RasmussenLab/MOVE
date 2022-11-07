0505f46d10e2db903bf0494ed6b17ef4576f8f13
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-11-07 16:27:14 +0100

    :bookmark: Update version number

--- src/move/__init__.py
+++ src/move/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 __license__ = "MIT"
-__version__ = (1, 2, 0)
-__all__ = ["conf", "data", "HYDRA_VERSION_BASE", "models", "training", "VAE"]
+__version__ = (1, 2, 1)
+__all__ = ["conf", "data", "models", "training", "VAE"]
 
 HYDRA_VERSION_BASE = "1.2"
 

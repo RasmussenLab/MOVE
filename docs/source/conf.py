52a0fe05c52d7a6b2a3d41cd8943ee1159166f6b
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-11-08 16:43:13 +0100

    :heavy_plus_sign: :wrench: Add/configure Sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,35 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'move-dl'
-copyright = '2022, Valentas Brasas, Ricardo Hernandez Medina'
-author = 'Valentas Brasas, Ricardo Hernandez Medina'
-release = '1.0.0'
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path("../src").resolve()))
+
+project = "move-dl"
+copyright = "2022, Valentas Brasas, Ricardo Hernandez Medina"
+author = "Valentas Brasas, Ricardo Hernandez Medina"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = []
+
+# -- Napoleon settings --------------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#configuration
+
+napoleon_google_docstring = True

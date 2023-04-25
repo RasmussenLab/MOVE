48db9d7da156034730bd605609ec1b3ba2d5e3c8
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2023-04-25 12:37:46 +0200

    :bug: Create parents of interim data directory

--- src/move/tasks/identify_associations.py
+++ src/move/tasks/identify_associations.py
@@ -303,8 +303,6 @@ def identify_associations(config: MOVEConfig):
         b_df = pd.DataFrame(dict(feature_b_name=con_names))
         b_df.index.name = "feature_b_id"
         b_df.reset_index(inplace=True)
-        results = (
-            results
             .merge(a_df, on="feature_a_id", how="left")
             .merge(b_df, on="feature_b_id", how="left")
         )

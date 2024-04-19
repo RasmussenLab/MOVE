6330e927414ca61c4f99e0ea880987fec22d28b9
LFT18 <143485442+LFT18@users.noreply.github.com>
2024-04-19 16:29:44 +0200

    Update __init__.py multiprocessing

--- src/move/tasks/__init__.py
+++ src/move/tasks/__init__.py
@@ -3,9 +3,12 @@ __all__ = [
     "encode_data",
     "identify_associations",
     "tune_model",
+    "identify_associations_multiprocess"
 ]
 
 from move.tasks.analyze_latent import analyze_latent
 from move.tasks.encode_data import encode_data
 from move.tasks.identify_associations import identify_associations
 from move.tasks.tune_model import tune_model
+from move.tasks.identify_associations_multiprocess import identify_associations_multiprocess
+from move.tasks.analyze_latent_fast import analyze_latent_fast

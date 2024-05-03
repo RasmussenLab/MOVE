:bug: :construction: Fix metrics calculation

src/move/models/base.py
@@ -4,7 +4,7 @@ import inspect
 from abc import ABC, abstractmethod
 from importlib import import_module
 from pathlib import Path
-from typing import Any, OrderedDict, Type, TypedDict, TypeVar, cast
+from typing import Any, OrderedDict, Type, TypedDict, TypeVar, Union, cast, overload
 
 import torch
 from torch import nn
@@ -68,9 +68,21 @@ class BaseVae(nn.Module, ABC):
         """Create latent representation."""
         ...
 
+    @overload
+    @abstractmethod
+    def reconstruct(
+        self, batch: torch.Tensor, as_one: bool = False
+    ) -> tuple[torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    @abstractmethod
+    def reconstruct(self, batch: torch.Tensor, as_one: bool = True) -> torch.Tensor: ...
+
     @torch.no_grad()
     @abstractmethod
-    def reconstruct(self, batch: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def reconstruct(
+        self, batch: torch.Tensor, as_one: bool = False
+    ) -> Union[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
         """Create reconstruction."""
         ...
 

src/move/models/vae.py
@@ -127,8 +127,10 @@ class Vae(BaseVae):
     def project(self, batch: torch.Tensor) -> torch.Tensor:
         return self.encode(batch)[0]
 
-    def reconstruct(self, batch: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def reconstruct(self, batch: torch.Tensor, as_one: bool = False):
         out = self(batch)["x_recon"]
+        if as_one:
+            return out
         out_disc, out_cont = self.split_output(out)
         recon_disc = torch.cat(
             [logits.flatten(start_dim=1) for logits in out_disc], dim=1

src/move/models/vae_distribution.py
@@ -140,14 +140,17 @@ class VaeDistribution(BaseVae):
     def project(self, batch: torch.Tensor) -> torch.Tensor:
         return self.encode(batch)[0]
 
-    def reconstruct(self, batch: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    def reconstruct(self, batch: torch.Tensor, as_one: bool = False):
         out = self(batch)["x_recon"]
         out_disc, out_cont = self.split_output(out)
         out_cont = cast(ContinuousDistribution, out_cont)
         recon_disc = torch.cat(
             [logits.flatten(start_dim=1) for logits in out_disc], dim=1
         )
+        # get location only (mean)
         recon_cont = torch.cat([args["loc"] for args in out_cont], dim=1)
+        if as_one:
+            return torch.cat((recon_disc, recon_cont), dim=1)
         return recon_disc, recon_cont
 
     def forward(self, x: torch.Tensor) -> VaeOutput:
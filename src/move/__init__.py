a7e62754d34b5d07ad4013adac59a69d5345e57a
Henry <henry.webel@cpr.ku.dk>
2024-05-16 18:06:54 +0200

    :bug: :rewind: HYDRA_VERSION_BASE needed for imports

--- src/move/__init__.py
+++ src/move/__init__.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-from move.training.training_loop import training_loop
-from move.models.vae import VAE
-from move import conf, data, models
+
+HYDRA_VERSION_BASE = "1.2"
+
+from move import conf, data, models  # noqa:E402
+from move.models.vae import VAE  # noqa:E402
+from move.training.training_loop import training_loop  # noqa:E402
 
 __license__ = "MIT"
 __version__ = (1, 4, 10)
 __all__ = ["conf", "data", "models", "training_loop", "VAE"]
-
-HYDRA_VERSION_BASE = "1.2"

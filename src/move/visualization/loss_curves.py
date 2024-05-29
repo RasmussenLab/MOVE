28a5bdedf57ef47944d1ac90acfc5e6729864a7e
Ricardo Hernández Medina <ricardo.hernandez.medina@sund.ku.dk>
2024-05-29 14:27:22 +0200

    :art: Update labels and type hints

--- src/move/visualization/loss_curves.py
+++ src/move/visualization/loss_curves.py
@@ -1,12 +1,13 @@
 __all__ = ["LOSS_LABELS", "plot_loss_curves"]
 
 from collections.abc import Sequence
+from typing import Union
 
 import matplotlib.figure
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from move.visualization.figure import create_figure
 from move.visualization.style import (
     DEFAULT_PLOT_STYLE,
     DEFAULT_QUALITATIVE_PALETTE,
@@ -14,11 +15,16 @@ from move.visualization.style import (
     style_settings,
 )
 
-LOSS_LABELS = ("Loss", "Cross-Entropy", "Sum of Squared Errors", "KLD")
+LOSS_LABELS = (
+    "Loss",
+    "Reconstruction error (discrete)",
+    "Reconstruction error (continuous)",
+    "Regularization error",
+)
 
 
 def plot_loss_curves(
-    losses: Sequence[list[float]],
+    losses: Union[Sequence[list[float]], pd.DataFrame],
     labels: Sequence[str] = LOSS_LABELS,
     style: str = DEFAULT_PLOT_STYLE,
     colormap: str = DEFAULT_QUALITATIVE_PALETTE,
@@ -27,7 +33,7 @@ def plot_loss_curves(
     """Plot one or more loss curves.
 
     Args:
-        losses: List containing lists of loss values
+        losses: List containing lists of loss values or a DataFrame
         labels: List containing names of each loss line
         style: Name of style to apply to the plot
         colormap: Name of colormap to use for the curves
@@ -35,11 +41,17 @@ def plot_loss_curves(
     Returns:
         Figure
     """
+    is_df = isinstance(losses, pd.DataFrame)
     num_epochs = len(losses[0])
     epochs = np.arange(num_epochs)
     with style_settings(style), color_cycle(colormap):
-        fig, ax = plt.subplots()
-        for loss, label in zip(losses, labels):
+        fig, ax = create_figure()
+        for i, label in enumerate(labels):
+            if is_df:
+                colname = losses.columns[i]
+                loss = losses[colname]
+            else:
+                loss = losses[i]
             ax.plot(epochs, loss, label=label, linestyle="-")
         ax.legend()
         ax.set(xlabel=xlabel, ylabel="Loss")

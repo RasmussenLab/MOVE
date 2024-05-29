2584720abc488bec3a1ea81a1fdae2feb355df05
Ricardo Hernández Medina <ricardo.hernandez.medina@sund.ku.dk>
2024-05-29 17:07:20 +0200

    :construction: Allow fig kwargs

--- src/move/visualization/figure.py
+++ src/move/visualization/figure.py
@@ -5,14 +5,14 @@ from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 
-def create_figure() -> tuple[Figure, Axes]:
+def create_figure(**fig_kw) -> tuple[Figure, Axes]:
     """Create a figure.
 
     Returns:
         A tuple containing a Figure and an Axes object. Unlike the customary
         (and equivalent) `matplotlib.pyplot.subplots` function, this method is
         correctly typed. That's the only difference."""
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(**fig_kw)
     assert isinstance(fig, Figure)
     assert isinstance(ax, Axes)
     return fig, ax

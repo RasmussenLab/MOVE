0464fc69412dba02ed27ebadc8cae5f176c8fea7
Ricardo Hernández Medina <ricardo.hernandez.medina@sund.ku.dk>
2024-07-12 18:38:32 +0200

    :construction: Create splitting module

---
+++ src/move/data/splitting.py
@@ -0,0 +1,35 @@
+__all__ = ["split_samples"]
+
+import torch
+
+
+def split_samples(
+    num_samples: int,
+    train_frac: float = 0.9,
+    test_frac: float = 0.1,
+    valid_frac: float = 0.0,
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Randomly split samples into training, test, and validation sets.
+
+    Args:
+        num_samples: Number of samples to split.
+        train_frac: Fraction of samples corresponding to training set.
+        test_frac: Fraction of samples corresponding to test set.
+        valid_frac: Fraction of samples corresponding to validation set.
+
+    Returns:
+        Tuple containing indices corresponding to each subset.
+    """
+    if (train_frac + test_frac + valid_frac) != 1.0:
+        raise ValueError("The sum of the subset fractions must be equal to one.")
+
+    train_size = int(train_frac * num_samples)
+    test_size = int(test_frac * num_samples)
+    valid_size = num_samples - train_size - test_size
+
+    perm = torch.randperm(num_samples)
+
+    tup = torch.split(perm, (train_size, test_size, valid_size))
+    out = tuple(map(torch.msort, tup))
+    assert len(out) == 3
+    return out
